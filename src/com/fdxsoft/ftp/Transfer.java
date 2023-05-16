87f0f88eeb6c623c40e2949051adae3bf8bc14f8
Federico Martinez E <fedemtzesc@hotmail.com>
2023-05-15 21:03:47 -0500

    Le agregue el autor

--- src/com/fdxsoft/ftp/Transfer.java
+++ src/com/fdxsoft/ftp/Transfer.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Autor Federico Martinez E.
  */
 package com.fdxsoft.ftp;
 
